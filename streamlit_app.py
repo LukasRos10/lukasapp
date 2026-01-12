4e4ff6d2cdb142a3835001a8a4807b417e779185
LukasRos10 <lukasrosenberg2010@outlook.dk>
2026-01-12 22:51:39 +0000

    Opdateret hjemmeside

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Min hjemmeside")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("følg dette link til youtube [https://www.youtube.com/]")
